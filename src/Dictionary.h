58625af7531b7ba10b15b24626936292a7be2caa
XxShenglixX <c_shengli@hotmail.com>
2014-11-11 20:56:34 +0800

    Added Dictionary.h

---
+++ src/Dictionary.h
@@ -0,0 +1,13 @@
+#ifndef Dictionary_H
+#define Dictionary_H
+
+typedef struct Dictionary Dictionary;
+
+struct Dictionary
+{
+    char *entry ;
+    int sizeofEntry ;
+};
+
+
+#endif // Dictionary_H
